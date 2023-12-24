01fd698bdc84db70a1402668acc4e1e2afdbfb3f
Anton <a.chertok@akatech.com.ua>
2023-12-24 11:20:01 +0000

    init project

---
+++ src/router.tsx
@@ -0,0 +1,32 @@
+import Layout from "components/Layout";
+import { lazy } from "react";
+import { createBrowserRouter, Navigate } from "react-router-dom";
+
+// const page = lazy(() => import("./page-path"));
+
+export const ROUTES = {
+  HOME: "/",
+};
+
+const routes = [
+  {
+    path: ROUTES.HOME,
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to={ROUTES.HOME} replace={true} />,
+      },
+      {
+        // path: ROUTES.PAGE,
+        // element: <Page />,
+      },
+    ],
+  },
+];
+
+const router = createBrowserRouter(routes, {
+  basename: "/sporty",
+});
+
+export default router;
