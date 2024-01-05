8d0ef113917eb801dcda8f06f1d630cd579eee51
Anton <a.chertok@akatech.com.ua>
2024-01-05 23:21:51 +0000

    customized buttons and inputs

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,7 @@
-import { Box, Button } from "@mui/material";
+import { Box, Button, FormControl, TextField } from "@mui/material";
 import React, { FC } from "react";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 interface IHomeProps {
   //...
@@ -11,10 +12,23 @@ const Home: FC<IHomeProps> = () => {
     <>
       <Box sx={{ backgroundColor: "#242424", color: "white" }}>
         Home component
+        <Button variant="text" endIcon={<ArrowForwardIcon />}>
+          Start
+        </Button>
         <Button variant="contained" endIcon={<FavoriteBorderOutlinedIcon />}>
           Add to favorites
         </Button>
         <Button variant="outlined">Give a rating</Button>
+        <FormControl>
+          <TextField variant="outlined" placeholder="Email" type="email" />
+
+          <TextField
+            variant="outlined"
+            placeholder="Your comment"
+            multiline
+            rows={4}
+          />
+        </FormControl>
       </Box>
     </>
   );
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -23,25 +23,42 @@ export const theme = createTheme({
     //   defaultProps: { disableRipple: true },
     // },
 
+    MuiTextField: {
+      styleOverrides: {
+        root: ({ ownerState }: { ownerState: any }) => ({
+          ...(ownerState.variant === "outlined" && {
+            backgroundColor: "transparent",
+            borderRadius: 30,
+            border: "1px solid",
+            borderColor: "#ffffff",
+
+            ".MuiOutlinedInput-root .MuiOutlinedInput-input": {
+              color: "#ffffff",
+            },
+
+            "& .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline": {
+              borderColor: "transparent",
+            },
+            "& .MuiOutlinedInput-root.Mui-focused  .MuiOutlinedInput-notchedOutline":
+              {
+                borderColor: "transparent",
+              },
+          }),
+
+          ...(ownerState.multiline && {
+            borderRadius: 15,
+          }),
+        }),
+      },
+    },
+
     MuiButton: {
       defaultProps: {
         disableRipple: true,
         disableElevation: true,
       },
-      styleOverrides: {
-        // root: {
-        // textTransform: "none",
-        // borderColor: "#ffffff",
-        // borderRadius: 30,
-        // color: "#ffffff",
-        // backgroundColor: "transparent",
-        // "&:hover": {
-        //   backgroundColor: "#ffffff",
-        //   borderColor: "#ffffff",
-        //   color: "#000000",
-        // },
-        // },
 
+      styleOverrides: {
         root: ({ ownerState }: { ownerState: any }) => ({
           ...{
             textTransform: "none",
@@ -65,6 +82,15 @@ export const theme = createTheme({
             "&:hover": {
               backgroundColor: "transparent",
               borderColor: "#ffffff",
+              color: "#ffffff",
+            },
+          }),
+
+          ...(ownerState.variant === "text" && {
+            color: "#000000",
+
+            "&:hover": {
+              backgroundColor: "#000000",
               color: "#ffffff",
             },
           }),
