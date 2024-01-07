b6d1e66afdf0fb12ca41df43e42f084b1bd23886
Anton <a.chertok@akatech.com.ua>
2024-01-07 00:10:55 +0000

    Update Favorites.tsx

--- src/components/Favorites/Favorites.tsx
+++ src/components/Favorites/Favorites.tsx
@@ -1,3 +1,4 @@
+import { Box, Container, Stack } from "@mui/material";
 import React, { FC } from "react";
 
 interface IFavoritesProps {
@@ -5,7 +6,28 @@ interface IFavoritesProps {
 }
 
 const Favorites: FC<IFavoritesProps> = () => {
-  return <div>Favorites page </div>;
+  return (
+    <Box component="section">
+      <Container sx={{ outline: "1px solid tomato" }}>
+        <Stack direction={{ xs: "column", xl: "row" }} spacing={{ xl: 16 }}>
+          <Box sx={{ outline: "1px solid green", width: "100%" }}>Quotes</Box>
+
+          <Box
+            sx={{
+              width: {
+                xs: "100%",
+                xl: 850,
+              },
+              flexShrink: 0,
+              outline: "1px solid red",
+            }}
+          >
+            Cards
+          </Box>
+        </Stack>
+      </Container>
+    </Box>
+  );
 };
 
 export default Favorites;
