de6adf788b28c74987a550e4d842091389b49470
Anton <a.chertok@akatech.com.ua>
2024-03-16 11:04:37 +0000

    fix():trim-text

--- src/utils/trimText.test.ts
+++ src/utils/trimText.test.ts
@@ -1,24 +1,7 @@
 import { trimText } from "./trimText";
 
-// console.log(
-//   trimText(
-//     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum",
-//     10
-//   )
-// );
-
 describe("Trim text function testing", () => {
-  test("Throw error if trim length less than 4", () => {
-    const str =
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum";
-    const trimLength = 3;
-
-    expect(() => {
-      trimText(str, trimLength);
-    }).toThrow(new Error("Trim length must not be less than 4"));
-  });
-
-  test("Return trimmed text with total 10 symbols", () => {
+  test("Valid trim result", () => {
     const str =
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum";
     const trimLength = 10;
@@ -26,18 +9,33 @@ describe("Trim text function testing", () => {
     expect(trimText(str, trimLength)).toEqual("Lorem I...");
   });
 
-  test("Return full string", () => {
+  test("Return full string on str.length less than trimLength", () => {
     const str = "Lorem I";
     const trimLength = 10;
 
     expect(trimText(str, trimLength)).toEqual(str);
   });
 
-  test("Return untrimmed string", () => {
-    const str =
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum";
-    // const trimLength = 10;
+  test("Return undefined on str=null || undefined", () => {
+    const str = null;
+    const trimLength = 10;
 
-    expect(trimText(str, undefined)).toEqual(str);
+    expect(trimText(str, trimLength)).toEqual(undefined);
+  });
+
+  test("Return full string on trimLength = null||undefined", () => {
+    const str =
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry";
+    const trimLength = null;
+
+    expect(trimText(str, trimLength)).toEqual(str);
+  });
+
+  test("Return full string on trimLength <= 3", () => {
+    const str =
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry";
+    const trimLength = 3;
+
+    expect(trimText(str, trimLength)).toEqual(str);
   });
 });
--- src/utils/trimText.ts
+++ src/utils/trimText.ts
@@ -1,6 +1,8 @@
-export const trimText = (str: string, length: number | undefined) => {
-  if (!length) return str;
-  if (length <= 3) throw new Error("Trim length must not be less than 4");
-  if (str.length <= length - 3) return str;
+export const trimText = (
+  str: string | null | undefined,
+  length: number | null | undefined = 10
+) => {
+  if (!str) return;
+  if (!length || str.length <= length - 3 || length <= 3) return str;
   return str.slice(0, length - 3).trim() + "...";
 };
