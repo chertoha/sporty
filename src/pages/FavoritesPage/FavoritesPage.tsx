6cca8369640b574f120589f73009309f80dcd02e
Anton <a.chertok@akatech.com.ua>
2024-09-19 14:36:17 +0100

    feat: favicon

--- src/pages/FavoritesPage/FavoritesPage.tsx
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -1,7 +1,19 @@
 import Favorites from "components/Favorites";
+import { Helmet } from "react-helmet-async";
 
 const FavoritesPage = () => {
-  return <Favorites />;
+  return (
+    <>
+      <Helmet>
+        <title>Favorites</title>
+        <meta
+          name="description"
+          content="sport exercises webb app favorites page"
+        />
+      </Helmet>
+      <Favorites />;
+    </>
+  );
 };
 
 export default FavoritesPage;
